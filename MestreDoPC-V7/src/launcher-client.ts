jeanavila997-ux/Mestre-do-PC-V7

refactor: modularize MestreDoPC-V7 MCP server architecture
- Extract config, domain, infra, security, and MCP layers
- Create BaseTool abstraction with 6 tool implementations
- Add launcher HTTP client, retry, and poller utilities
- Add Ollama client for IA tool
- Refactor index.ts to pure bootstrap
- Add GitHub Actions CI workflow
- Fix lint errors and TypeScript strict mode issues

Co-authored-by: Copilot <223556219+Copilot@users.noreply.github.com>

MestreDoPC-V7/src/launcher-client.ts
@@ -7,9 +7,9 @@
 
 import { execFile } from 'child_process';
 import { promisify } from 'util';
-import { logger } from './logger.js';
-import { encodeCommand, detectInjection } from './security/sanitizer.js';
-import { validateToolParams } from './security/whitelist.js';
+import { logger } from './logger';
+import { encodeCommand, detectInjection } from './security/sanitizer';
+import { validateToolParams } from './security/whitelist';
 
 const execFileAsync = promisify(execFile);
 
@@ -97,11 +97,9 @@ if ($vol) {
 
 export async function executeLauncherCommand(
   toolName: string,
-  params: Record<string, string>,
-  correlationId?: string
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-): Promise<any> {
-  const requestLogger = logger.child({ correlationId, toolName, params: Object.keys(params) });
+  params: Record<string, string>
+): Promise<unknown> {
+  const requestLogger = logger.child({ toolName, params: Object.keys(params) });
   const executionStart = Date.now();
 
   const validation = validateToolParams(toolName, params);
@@ -130,11 +128,10 @@ export async function executeLauncherCommand(
   const psScript = scriptFn(params);
   const encoded = encodeCommand(psScript);
 
-  // Retrieve configuration from environment or defaults
   const timeoutMs = parseInt(process.env.LAUNCHER_TIMEOUT || '30000', 10);
   const maxAttempts = parseInt(process.env.LAUNCHER_RETRIES || '3', 10);
   let attempt = 1;
-  let delay = 1000; // Starting delay of 1s for exponential backoff
+  let delay = 1000;
 
   while (attempt <= maxAttempts) {
     const attemptStart = Date.now();
@@ -160,39 +157,37 @@ export async function executeLauncherCommand(
       }
 
       requestLogger.info(
-        { 
-          success: true, 
-          attempt, 
+        {
+          success: true,
+          attempt,
           attemptDurationMs: Date.now() - attemptStart,
-          totalDurationMs: Date.now() - executionStart 
-        }, 
+          totalDurationMs: Date.now() - executionStart,
+        },
         'Tool execution finished'
       );
-      
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+
       return result;
     } catch (error) {
       const errorMessage = error instanceof Error ? error.message : 'Unknown error';
-      
+
       requestLogger.warn(
-        { 
-          error: errorMessage, 
-          attempt, 
+        {
+          error: errorMessage,
+          attempt,
           attemptDurationMs: Date.now() - attemptStart,
-          willRetry: attempt < maxAttempts 
-        }, 
+          willRetry: attempt < maxAttempts,
+        },
         'PowerShell execution attempt failed'
       );
 
       if (attempt === maxAttempts) {
         requestLogger.error(
-          { error: errorMessage, totalDurationMs: Date.now() - executionStart }, 
+          { error: errorMessage, totalDurationMs: Date.now() - executionStart },
           'All PowerShell execution attempts failed'
         );
         throw new Error(`PowerShell execution failed: ${errorMessage}`);
       }
 
-      // Wait with exponential backoff before the next attempt
       await new Promise(resolve => setTimeout(resolve, delay));
       delay *= 2;
       attempt++;

avila2026/AgentFLOW

MestreDoPC-V7/src/launcher-client.ts
@@ -5,9 +5,9 @@
  * to execute PowerShell commands safely.
  */
 
-import { logger } from './logger.js';
-import { buildSafeCommand, detectInjection } from './security/sanitizer.js';
-import { validateToolParams } from './security/whitelist.js';
+import { logger } from './logger';
+import { buildSafeCommand, detectInjection } from './security/sanitizer';
+import { validateToolParams } from './security/whitelist';
 
 /**
  * Launcher configuration
@@ -30,7 +30,7 @@ interface RunResponse {
  */
 interface RunStatusResponse {
   status: 'running' | 'completed' | 'failed';
-  result?: any;
+  result?: unknown;
   error?: string;
 }
 
@@ -131,7 +131,7 @@ async function pollForCompletion(
 export async function executeLauncherCommand(
   toolName: string,
   params: Record<string, string>
-): Promise<any> {
+): Promise<unknown> {
   const requestLogger = logger.child({ toolName, params: Object.keys(params) });
 
   // Validate parameters against whitelist